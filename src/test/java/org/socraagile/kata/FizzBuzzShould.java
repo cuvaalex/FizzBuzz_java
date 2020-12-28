40c0e04b71ff44854cdad1d4139761cda7c91f66
Alexandre Cuva <alexandre.cuva@gmail.com>
2020-12-28 21:30:18 +0100

    empty message

--- src/test/java/org/socraagile/kata/FizzBuzzShould.java
+++ src/test/java/org/socraagile/kata/FizzBuzzShould.java
@@ -10,12 +10,12 @@ import static org.assertj.core.api.Assertions.*;
 
 public class FizzBuzzShould {
 
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
     @DisplayName("Return the string of the number")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 4})
     void return_the_string_of_the_number(int number) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String result = fizzBuzz.compute(number);
 
         assertThat(result).isEqualTo(String.valueOf(number));
@@ -25,8 +25,6 @@ public class FizzBuzzShould {
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9})
     void return_Fizz_when_number_is_multiple_of_3(int number) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String result = fizzBuzz.compute(number);
 
         assertThat(result).isEqualTo("Fizz");
@@ -36,11 +34,19 @@ public class FizzBuzzShould {
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20})
     void return_Buzz_when_number_is_multiple_of_5(int number) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         String result = fizzBuzz.compute(number);
 
         assertThat(result).isEqualTo("Buzz");
+    }
+
+    @DisplayName("Return FizzBuzz when number is 15")
+    @Test
+    public void return_FizzBuZZ_when_number_is_15() {
+        int number = 15;
+
+        String result = fizzBuzz.compute(number);
+
+        assertThat(result).isEqualTo("FizzBuzz");
     }
 
 }
