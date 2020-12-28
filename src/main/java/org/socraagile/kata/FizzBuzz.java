1bd3a5de5dd3f19c0431033fe1dcae6dc4a7b008
Alexandre Cuva <alexandre.cuva@gmail.com>
2020-12-28 21:30:58 +0100

    empty message

--- src/main/java/org/socraagile/kata/FizzBuzz.java
+++ src/main/java/org/socraagile/kata/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.socraagile.kata;
 
 public class FizzBuzz {
     public String compute(int number) {
+        if(number == 15){
+            return "FizzBuzz";
+        }
         if(number % 3 == 0){
             return "Fizz";
         }
